bbb4310c52fad43ba46ba116ad8bfcef0f6b7797
Wizkido Junior <72566129+RieBerry@users.noreply.github.com>
2023-04-02 10:53:58 +0000

    RieBerry-Test

---
+++ nodemodules/index.js
@@ -0,0 +1,21 @@
+{
+
+  "name": "nodemodules",
+
+  "version": "1.0.0",
+
+  "description": "",
+
+  "main": "index.js",
+
+  "scripts": {
+
+    "test": "echo \"Error: no test specified\" && exit 1"
+
+  },
+
+  "author": "Rie Berry",
+
+  "license": "ISC"
+
+}
